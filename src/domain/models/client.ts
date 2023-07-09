93de5e534686aa736f11c086c0530ff26fece673
IveTou <ive_tourinho@hotmail.com>
2023-07-09 12:35:50 -0300

    feat: data definition

---
+++ src/domain/models/client.ts
@@ -0,0 +1,6 @@
+export type Client = {
+    id: string
+    firstName: string
+    job: string
+    jobDescriptor: string
+}
---
+++ src/domain/models/market.ts
@@ -0,0 +1,10 @@
+export type Market = {
+    accountName: string
+    amount: number
+    creditCardCCV: number
+    creditCardIssuer: string
+    creditCardNumber: number
+    currencyName: string
+    transactionDescription: string
+    trasactionType: string //consider using ENUM in the future
+}
